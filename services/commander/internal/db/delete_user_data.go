f5dde305ced9aad8e2428fb0f76393fe050d95cf
Yannis Matezki <git@yannismate.de>
2024-06-09 22:14:36 +0200

    Close db statements after use

--- services/commander/internal/db/delete_user_data.go
+++ services/commander/internal/db/delete_user_data.go
@@ -3,26 +3,31 @@ package db
 import "context"
 
 func (m *mainDB) DeleteUserData(ctx context.Context, twitchUserID string) error {
-	_, err := m.dbPool.Query(ctx, "delete from "+
+	rows, err := m.dbPool.Query(ctx, "delete from "+
 		"event_sub_subscriptions "+
 		"where "+
 		"twitch_user_id = $1;", twitchUserID)
 	if err != nil {
 		return err
 	}
+	rows.Close()
 
-	_, err = m.dbPool.Query(ctx, "delete from "+
+	rows, err = m.dbPool.Query(ctx, "delete from "+
 		"bot_commands "+
 		"where "+
 		"twitch_user_id = $1;", twitchUserID)
 	if err != nil {
 		return err
 	}
+	rows.Close()
 
-	_, err = m.dbPool.Query(ctx, "delete from "+
+	rows, err = m.dbPool.Query(ctx, "delete from "+
 		"bot_users "+
 		"where "+
 		"twitch_user_id = $1;", twitchUserID)
+	if err == nil {
+		rows.Close()
+	}
 
 	return err
 }
