997d4ee0aebc57b5074970491eb1deb63408f91d
Yannis Matezki <git@yannismate.de>
2024-06-09 17:57:42 +0200

    Log HTTP requests only when no error occurred

--- services/commander/internal/util/logging_roundtripper.go
+++ services/commander/internal/util/logging_roundtripper.go
@@ -10,7 +10,7 @@ type LoggingRoundTripper struct{}
 
 func (lrt *LoggingRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 	res, err := http.DefaultTransport.RoundTrip(req)
-	if err != nil {
+	if err == nil {
 		if req.Context().Value("trace-id") != nil {
 			log.Ctx(req.Context()).Info().Msg(fmt.Sprint("[HTTP Client] ", req.Method, " ", req.URL.String(), " - ", res.StatusCode))
 		} else {
