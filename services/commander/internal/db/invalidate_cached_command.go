dd3fc61e9274380e7551cfd5fcdd8ae2a83e1613
Yannis Matezki <git@yannismate.de>
2024-07-07 13:03:32 +0200

    Fix command cache invalidation

--- services/commander/internal/db/invalidate_cached_command.go
+++ services/commander/internal/db/invalidate_cached_command.go
@@ -3,7 +3,7 @@ package db
 import "context"
 
 func (c *cacheDB) InvalidateCachedCommand(ctx context.Context, channelID string, commandName string) error {
-	cacheKey := cachePrefixRanks + ":" + channelID + ":" + commandName
+	cacheKey := cachePrefixCommands + ":" + channelID + ":" + commandName
 	res := c.client.Del(ctx, cacheKey)
 	return res.Err()
 }
